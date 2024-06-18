cec53fff5273ce11769ec91f1df9446dc913457b
Rishi Labaniya <rishirajlabaniya@gmail.com>
2024-06-18 14:09:56 +0530

    text changed

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>w
         <p>
-         Rishi
+         Rishi Labaniya
         </p>
       </div>
       <p className="read-the-docs">
